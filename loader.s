cc7ced0992dd88d259a3b494d00cc07a129d6321
Antti Keränen <detegr@gmail.com>
2013-05-05 18:02:49 +0300

    Strange video memory touching seems to make it work...

--- loader.s
+++ loader.s
@@ -1,29 +1,30 @@
+;global kernel
 extern kmain
 
 ; Multiboot header
-MODULEALIGN equ 0x1				; Align modules on page boundaries
-MEMINFO		equ 0x2				; Information on available memory must be included
+MODULEALIGN equ 0x1					; Align modules on page boundaries
+MEMINFO		equ 0x2					; Information on available memory must be included
 FLAGS		equ MODULEALIGN|MEMINFO
-MAGIC		equ 0x1BADB002		; Multiboot header magic
-CHECKSUM	equ -(MAGIC + FLAGS)
-STACKSIZE	equ 0x4000			; 16k stack size
+MAGIC		equ 0x1BADB002			; Multiboot header magic
+CHECKSUM	equ 0-(MAGIC + FLAGS)
 
-section .text
-align 4							; Header must be 32-bit aligned
+section .text align = 4
 
 ; Allocate multiboot header
 dd MAGIC
 dd FLAGS
 dd CHECKSUM
 
-_start:
+STACKSIZE	equ 0x4000			; 16k stack size
+
+kernel:
 	mov esp, stack+STACKSIZE	; Setup stack pointer to the bottom of the stack
+	mov dword [0xb8000], 0x0	; Initializes video memory
 	call kmain					; Start kernel
 	cli							; Clear interrupts (when kernel exits)
 .halt:
-	hlt							; And halt forever
+	hlt
 	jmp .halt
 
-section .bss
-align 4 ; Align 32-bit
+section .bss align = 4
 stack: resb STACKSIZE ; 16k stack on 32-bit alignment
