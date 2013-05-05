c3311f8674f9e728547bb3f8bc32a608aeb2aa59
Antti Keränen <detegr@gmail.com>
2013-05-05 18:03:37 +0300

    printk for kernel

--- kernel.c
+++ kernel.c
@@ -1,4 +1,33 @@
+volatile unsigned char* video = (unsigned char*)0xB8000;
+
+static unsigned char row=0;
+static unsigned char line=0;
+
+void printk(const char* str)
+{
+	const char* s;
+	for(s=str; *s; s++)
+	{
+		char c=*s;
+		if(c=='\n')
+		{
+			row=0;
+			line++;
+			continue;
+		}
+		*(video+(line*0xA0)+row)=c;
+		*(video+(line*0xA0)+row+1)=0x07; // Gray on black
+		row+=2;
+		if(row>=0xA0)
+		{
+			row=0;
+			line++;
+		}
+	}
+}
+
 void kmain(unsigned long magic, unsigned long addr)
 {
-	while(1);
+	printk("Welcome to tapiOS!\n");
+	printk("You will be able to do...nothing!\n");
 }
