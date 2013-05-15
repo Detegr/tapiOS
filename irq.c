283a50fb13ab7e51be9f4e908f88dc5bbb633b19
Detegr <detegr@gmail.com>
2013-05-15 17:13:20 +0300

    irq.c & irq.h

---
+++ irq.c
@@ -0,0 +1,102 @@
+#include "irq.h"
+
+extern int _irq1_handler(void);
+
+void irq1_handler(void)
+{
+	printk("Interrupt test!\n");
+	inb(0x60);
+	uint8_t i=inb(0x61);
+	outb(0x61, i);
+}
+
+void setup_idt(void)
+{
+	printk("Setting up IDT...");
+	int i;
+	for(i=0; i<255; ++i)
+	{
+		if(i<=0x1F) idtentry(i, (uint32_t)&_noop_int, CODE_SELECTOR, GATE_INT32);
+		else if(i==0x21) idtentry(i, (uint32_t)&_irq1_handler, CODE_SELECTOR, GATE_INT32);
+		else if(i==0x27) idtentry(i, (uint32_t)&_spurious_irq_check_master, CODE_SELECTOR, GATE_INT32);
+		else if(i==0x2F) idtentry(i, (uint32_t)&_spurious_irq_check_slave, CODE_SELECTOR, GATE_INT32);
+		else idtentry(i, (uint32_t)0, 0, 0);
+	}
+	idtptr.limit=sizeof(idt)-1;
+	idtptr.base=(unsigned int)&idt;
+	_setidt();
+	printk("OK!\n");
+}
+
+#define PIC1 0x20
+#define PIC1_DATA 0x21
+#define PIC2 0xA0
+#define PIC2_DATA 0xA1
+
+#define ICW4_NEEDED 0x1
+#define ICW1_INIT 0x10
+
+#define ICW4_80X86_MODE 0x1
+
+#define EOI 0x20 // End Of Interrupt
+
+void remap_pic(uint8_t offset1, uint8_t offset2)
+{
+	outb(PIC1, ICW4_NEEDED|ICW1_INIT);
+	outb(PIC2, ICW4_NEEDED|ICW1_INIT);
+	outb(PIC1_DATA, offset1); // New offset (for first 8 IRQs)
+	outb(PIC2_DATA, offset2); // New offset (for the rest 8 IRQs)
+	outb(PIC1_DATA, 0x04); // New slave in IRQ2
+	outb(PIC2_DATA, 0x02); // Slave's master in IRQ1
+	outb(PIC1_DATA, ICW4_80X86_MODE);
+	outb(PIC2_DATA, ICW4_80X86_MODE);
+
+	outb(PIC1_DATA, 0xFD);
+	outb(PIC2_DATA, 0xFF);
+
+	__asm__ __volatile__("sti\n");
+}
+
+uint8_t pic_get_irq(void)
+{
+	outb(PIC1, 0x0B);
+	uint8_t data=inb(PIC1);
+	uint8_t i;
+	uint8_t bit;
+	for(i=data, bit=0; i!=0; i>>=1, bit++)
+	{
+		if(i & 0x1 && bit!=2) return bit;
+	}
+	data=inb(PIC2);
+	for(i=data; i!=0; i>>=1, bit++)
+	{
+		if(i & 0x1) return bit;
+	}
+	return 0xFF;
+}
+
+uint8_t pic1_get_isr(void)
+{
+	outb(PIC1, 0x0B); // Read ISR
+	return inb(PIC1);
+}
+
+uint8_t pic2_get_isr(void)
+{
+	outb(PIC2, 0x0B); // Read ISR
+	return inb(PIC2);
+}
+
+uint8_t is_spurious_irq_master(void)
+{
+	uint8_t isr=pic1_get_isr();
+	if(isr & 0x80) return 0;
+	else return 1;
+}
+
+uint8_t is_spurious_irq_slave(void)
+{
+	uint8_t isr=pic2_get_isr();
+	if(isr & 0x80) return 0;
+	else return 1;
+}
---
+++ irq.h
@@ -0,0 +1,18 @@
+#ifndef _TAPIOS_IRQ_H_
+#define _TAPIOS_IRQ_H_
+
+#include "util.h"
+#include "video.h"
+
+void irq1_handler(void);
+
+void setup_idt(void);
+void remap_pic(uint8_t offset1, uint8_t offset2);
+
+uint8_t pic_get_irq(void);
+uint8_t pic1_get_isr(void);
+uint8_t pic2_get_isr(void);
+uint8_t is_spurious_irq_master(void);
+uint8_t is_spurious_irq_slave(void);
+
+#endif
