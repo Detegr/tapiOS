4ea4c79460501c550546328c1b061eea13b1562a
Antti Keränen <detegr@gmail.com>
2013-05-19 12:16:39 +0300

    stuff

--- loader.s
+++ loader.s
@@ -1,3 +1,4 @@
+global _start
 extern kmain
 
 ; Paging stuff
@@ -5,7 +6,7 @@ KERNEL_VMA equ 0xC0000000
 PAGE_ENTRIES equ 1024
 
 ; From paging.s
-extern _enable_paging
+extern _map_kernel_to_higher_half
 
 ; Multiboot header
 MODULEALIGN equ 0x1				; Align modules on page boundaries
@@ -21,19 +22,28 @@ dd MAGIC
 dd FLAGS
 dd CHECKSUM
 
-page_directory:
-	times PAGE_ENTRIES dd 0
-page_tbl_low:
-	times PAGE_ENTRIES dd 0
-page_tbl_kernel:
-	times PAGE_ENTRIES dd 0
-
 STACKSIZE	equ 0x4000			; 16k stack size
 
+[section .setup]
 _start:
-
-kernel:
 	mov esp, stack+STACKSIZE	; Setup stack pointer to the bottom of the stack
+	sub esp, KERNEL_VMA			; Correct to the physical address of the stack
+	mov ebp, esp
+	
+	push page_tbl_kernel - KERNEL_VMA
+	push page_tbl_low - KERNEL_VMA
+	push page_directory - KERNEL_VMA
+	jmp _map_kernel_to_higher_half
+
+test:
+	push ebp
+	mov ebp, esp
+	mov eax, [ebp+4]
+	leave
+	ret
+
+[section .text]
+kernel:
 	cli							; Disable interrupts until we setup gdt and idt
 	call kmain					; Start kernel
 	cli							; Clear interrupts if kernel exists
@@ -43,3 +53,13 @@ kernel:
 
 section .bss align = 4
 stack: resb STACKSIZE ; 16k stack on 32-bit alignment
+
+[section .data]
+ALIGN 4096
+
+page_directory:
+	times PAGE_ENTRIES dd 0
+page_tbl_low:
+	times PAGE_ENTRIES dd 0
+page_tbl_kernel:
+	times PAGE_ENTRIES dd 0
--- paging.s
+++ paging.s
@@ -1,8 +1,115 @@
-global _enable_paging
+[section .setup]
+
+global _map_kernel_to_higher_half
+global _setup_page_table
 global _map_page
 
-_map_page:
+KERNEL_VMA equ 0xC0000000
+FLAG_PRESENT equ 0x1
+
+extern page_directory
+extern page_tbl_low
+extern page_tbl_kernel
+
+extern __kernel_start
+extern __kernel_end
+
+_map_kernel_to_higher_half:
+	pop eax 			; page_directory
+
+	mov ebx, 0x0
+	call _setup_page_table
+
+	mov ebx, 0xC0100000
+	call _setup_page_table
+
+	pop ebx 			; page_tbl_low
+	pop ecx 			; page_tbl_kernel
+
+	; Map low page (0x1000 - 0x100000)
+	; eax contains page_directory
+	mov ebx, 0x1000		; virtual
+	mov ecx, 1023		; count
+	mov edx, 0x1000		; physical
+	call _map_page
+
+	mov ebx, 0xC0100000	; virtual
+	mov ecx, 4096		; count
+	mov edx, 0x100000	; physical
+	call _map_page
+
 	ret
+
+_setup_page_table:
+	push eax
+	mov eax, ebx
+	call _calculate_directory_position ; to edx
+	pop eax
+	or ebx, FLAG_PRESENT
+	mov [eax + edx * 4], ebx
+	ret
+
+_map_page:
+	push ebp
+	mov esp, ebp
+
+	sub esp, 20
+	mov [esp-20], eax ; Directory
+	mov [esp-16], ebx ; Virtual address
+	mov [esp-12], ecx ; Count
+	mov [esp-8] , DWORD 0   ; Mapped bytes
+	mov [esp-4] , edx ; Physical address
+
+	mov eax, ebx
+	call _calculate_directory_position
+
+	mov eax, [esp-8] ; Original count
+	sub eax, ecx	; pages mapped
+	push ecx 		; Count
+	mov ecx, 4
+	mul ecx
+	mov ebx, edx 	; mapped bytes
+	pop ecx 		; remaining count
+	pop edx 		; directory index
+	pop eax 		; original count
+	add eax, ebx 	; directory + mapped bytes
+	pop ebx 		; virtual address
+
+	push ebx
+		push ecx
+		mov ecx, [eax + edx * 4] 	; get old entry
+		and ecx, FLAG_PRESENT
+		cmp ecx, 0x0				; if not present
+		je __panic 					; jump to panic
+		pop ecx
+	pop ebx
+	
+	mov [eax + edx * 4], ebx ; physical address to correct page table
+	or DWORD [eax + edx * 4], FLAG_PRESENT
+	add ebx, 0x1000 		 ; advance to next page
+
+	pop eax ; directory
+
+	loop _map_page
+
+	add esp, 20
+	leave
+	ret
+
+__panic:
+	hlt
+	jmp __panic
+
+_calculate_directory_position:
+	mov edx, eax
+	shr edx, 22 ; Index in page directory
+	ret
+
+_set_kernel_tbl_index:
+	or ebx, FLAG_PRESENT
+	mov [eax+edx], ebx ; Use index from edx
+	ret
+
 
 _enable_paging:
 	ret
