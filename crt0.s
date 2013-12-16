03eb00ef208fb84bd906d20820d68120281360d8
Antti Keränen <detegr@gmail.com>
2013-12-16 21:33:29 +0200

    Now zeroing bss in crt0.s

--- crt0.s
+++ crt0.s
@@ -1,6 +1,16 @@
 global _start
 extern main
+extern __bss_start
+extern __bss_end
+
 _start:
+	; Zero out bss
+	mov edi, __bss_start
+	mov ecx, __bss_end
+	sub ecx, edi
+	xor al, al
+	rep stosb
+
 	call main
 	mov ebx, eax
 	mov eax, 0x1
